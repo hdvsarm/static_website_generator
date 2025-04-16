18bcb2e08a9ba353b13d6c56b36fcbf98b04d692
hdvsarm <harrison.arms@gmail.com>
2025-04-15 23:01:42 -0500

    created function to determine md block type

--- src/blocks.py
+++ src/blocks.py
@@ -1,34 +1,40 @@
 from enum import Enum
 
-
-    # paragraph
-    # heading
-    # code
-    # quote
-    # unordered_list
-    # ordered_list
-
-
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
     CODE = "code"
     QUOTE = "quote"
-    UNORDERED_LIST = "unordered_list"
-    ORDERED_LIST = "ordered_list"
+    OLIST = "ordered_list"
+    ULIST = "unordered_list"
 
 
+def block_to_block_type(block):
+    lines = block.split("\n")
 
-# Headings start with 1-6 # characters, followed by a space and then the heading text.
-# Code blocks must start with 3 backticks and end with 3 backticks.
-# Every line in a quote block must start with a > character.
-# Every line in an unordered list block must start with a - character, followed by a space.
-# Every line in an ordered list block must start with a number followed by a . character and a space. The number must start at 1 and increment by 1 for each line.
-# If none of the above conditions are met, the block is a normal paragraph.
-
-
-def block_to_block_type(markdown):
-    pass
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    
+    return BlockType.PARAGRAPH
 
 
 def markdown_to_blocks(markdown):
